718b167cf9db6a6b6a3697a01c1279e13d5f5f01
elinthim <elin_thim@hotmail.com>
2023-04-05 13:53:47 +0200

    Add project files.

---
+++ Testinmvc/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Testinmvc.Models;
+
+namespace Testinmvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult CV()
+        {
+            return View();
+        }
+
+    }
+}
